0fd7603eb70f10ef114572ecbcb75ad5c0de46f6
Fabian Müller <fmueller@mpi-inf.mpg.de>
2018-06-08 15:10:09 -0700

    …

--- R/CommandRsystem-class.R
+++ R/CommandRsystem-class.R
@@ -91,8 +91,6 @@ setMethod("exec",
 		errFile <- path.expand(errFile)
 		
 		sysRes <- system2(getCmd(job), getArgs(job), wait=wait, stdout=logFile, stderr=errFile, ...)
-		print(getCmd(job))
-		print(getArgs(job))
 
 		sysOut <- character()
 		sysErr <- character()
